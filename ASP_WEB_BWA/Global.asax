c889b36bb4056332176dd96d7ff54462448738f4
Charana <Charana@CHAMP>
2017-05-09 11:25:32 +0530

    Add project files.

---
+++ ASP_WEB_BWA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_WEB_BWA.MvcApplication" Language="C#" %>
---
+++ ASP_WEB_BWA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_WEB_BWA.Startup))]
+namespace ASP_WEB_BWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
